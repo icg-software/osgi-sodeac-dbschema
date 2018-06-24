89f5d9a93019c7e3616fd729df74f6af277f9f9b
spalarus <s.palarus@googlemail.com>
2018-06-24 06:31:45 +0200

    fix some copyright sections

--- org.sodeac.dbschema.driver.oracle/src/main/java/org/sodeac/dbschema/driver/oracle/OracleDatabaseSchemaProvider.java
+++ org.sodeac.dbschema.driver.oracle/src/main/java/org/sodeac/dbschema/driver/oracle/OracleDatabaseSchemaProvider.java
@@ -1,7 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Sebastian Palarus
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sebastian Palarus - initial API and implementation
+ *******************************************************************************/
 package org.sodeac.dbschema.driver.oracle;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Map;
